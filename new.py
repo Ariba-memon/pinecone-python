c0dbda06ec624b9b46a4f8b398c1785742c4516b
Aribamemon <aribaabdulqadir@gmail.com>
2023-11-06 21:25:27 +0500

    first commit

---
+++ new.py
@@ -0,0 +1,14 @@
+# create an index with a single pod
+pip install pinecone-clientpinecone.create_index(index_name="chatbot", dimension=8)
+    
+#create an instance of your index
+index = pinecone.Index(index_name="chatbot")
+
+# upsert your vector embeddings
+index.upsert([
+    ("A", [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]),
+    ("B", [0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2]),
+    ("C", [0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3]),
+    ("D", [0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4]),
+    ("E", [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5])
+])
